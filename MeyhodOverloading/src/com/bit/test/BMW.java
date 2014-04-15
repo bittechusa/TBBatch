d3f506ea188be92441c8735d94a9e7d7604090ea
asadShalok <asadfrombronx@yahoo.com>
2014-04-15 19:59:48 -0400

    add project

---
+++ MeyhodOverloading/src/com/bit/test/BMW.java
@@ -0,0 +1,15 @@
+package com.bit.test;
+
+public class BMW extends Car
+{
+	public void move(Double x)
+	{
+		System.out.println("hello");
+	}
+	public void move(int x,int y)
+	{
+		
+	}
+
+
+}
---
+++ MeyhodOverloading/src/com/bit/test/Car.java
@@ -0,0 +1,15 @@
+package com.bit.test;
+
+public class Car 
+{
+	public void move(int x)
+	{
+		System.out.println("hi");
+	}
+
+	private int move(int x,int y)
+	{
+		System.out.println("hi");
+		return 5;
+	}
+}
