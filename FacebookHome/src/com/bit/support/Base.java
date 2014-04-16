dcd31ce84875b28e5feac4bbfe3d2aaeeffdc1a3
Kawkab Ahmed Reja <kawkabahmed@gmail.com>
2014-04-15 21:05:48 -0400

    add practice project

---
+++ FacebookHome/src/com/bit/support/Base.java
@@ -0,0 +1,14 @@
+package com.bit.support;
+
+import com.bit.test.SignUp;
+
+public class Base {
+
+	public static void main(String[] args) 
+	{
+		SignUp k=new SignUp();// object creation
+		k.p2m1();
+
+	}
+
+}
