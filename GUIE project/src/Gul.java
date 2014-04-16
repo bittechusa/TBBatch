a2c344c088b73f4fadf69d40bc8aac9005b34876
gul <gul_lisha@yahoo.com>
2014-04-15 20:05:11 -0400

    add project

---
+++ GUIE project/src/Gul.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class Gul {
+
+	public static void main(String[] args) {
+		Mur gul=new Mur();
+		gul.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gul.setSize(300, 200);
+		gul.setVisible(true);
+
+	}
+
+}
