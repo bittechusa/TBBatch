4cae178c3e649a3a2b4347e31b07dffc7d925ba1
shahidul islam <shahidulmail07@gmail.com>
2014-06-17 21:03:30 -0400

    add file

---
+++ AbstracPractice052014/src/com/bit/test/Demo.java
@@ -0,0 +1,15 @@
+package com.bit.test;
+
+public abstract class Demo 
+{
+	public void m1()
+	{
+		System.out.println("h1");
+	}
+	public abstract void m2();
+	public abstract void m3();
+	Demo()
+	{
+		System.out.println("cD");
+	}
+}
